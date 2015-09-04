d2edc8c4c873bdf6310697c2eefd377ae88de35b
Mikhail Ivanov <mivanov@mirantis.com>
2015-09-04 21:20:37 +0300

    [BROKEN] try to add teachers support

--- parser.rb
+++ parser.rb
@@ -15,119 +15,146 @@ class String
   end
 end
 
+class Parser
+  def get_pairs(string)
+    dict = {}
 
-def get_pairs(string)
-  dict = {}
-  if string.length == 1
-    dict
-  else
-    dict['lector'] = string.split(')')[1]
-    subj_room = string.split(')')[0]<<')'
-    room = subj_room[0..4]
-    subj_room[0..4] = ''
-    subj = subj_room
-    false_check = 0
-    subj.split('')[0..3].each do |i|
-      if i.is_lower?
-        false_check += 1
+    if string.nil? or string.length == 1
+      dict
+    else
+      dict['lector'] = string.split(')')[1]
+      subj_room = string.split(')')[0]<<')'
+      room = subj_room[0..4]
+      subj_room[0..4] = ''
+      subj = subj_room
+      false_check = 0
+      subj.split('')[0..3].each do |i|
+        if i.is_lower?
+          false_check += 1
+        end
+      end
+      if false_check == 2
+        room << subj[0]
+        subj[0] = ''
+      elsif false_check == 3 and subj[0].is_lower?
+        room << subj[0]
+        subj[0] = ''
+      end
+      dict['subject'] = subj
+      dict['room'] = room
+      dict
+    end
+  end
+
+  def rasp_pars(page)
+    response = Hash.new
+    first_week = Hash.new
+    second_week = Hash.new
+    a = []
+    week = {1 => 'monday', 2 => 'tuesday', 3 => 'wednesday', 4 => 'thursday', 5 => 'friday', 6 => 'saturday'}
+
+    html = Nokogiri::HTML(page.body.force_encoding('UTF-8'))
+    rasp = html.css('div.text-center')[0]
+
+    if rasp.content.split("\r\n")[1].strip.split(' ')[0] == 'Нечётная'
+      first_week['odd'] = true
+      flag = true
+    else
+      first_week['odd'] = false
+      flag = false
+    end
+
+    rasp = html.css('div.rasp-table-col')
+
+    rasp.each do |div|
+      answer = div.content.split("\r\n")
+      answer.each do |i|
+        answer[answer.index(i)] = i.strip
+      end
+      answer.delete_if(&:empty?)
+      answer.delete_if{|i| i.include?':' }
+      answer.delete_at(0)
+      a.push(answer)
+    end
+
+    a[0..5].each do |list|
+      dict = {}
+      for i in [0, 2, 4, 6]
+        dict[list[i]] = get_pairs(list[i+1])
+      end
+      first_week[week[a.find_index(list)+1]] = dict
+    end
+
+    if flag
+      second_week['odd'] = false
+    else
+      second_week['odd'] = true
+    end
+    index = 0
+    a[6..11].each do |list|
+      dict = {}
+      for i in [0, 2, 4, 6]
+        dict[list[i]] = get_pairs(list[i+1])
+      end
+      second_week[week[index+1]] = dict
+      index +=1
+    end
+
+    response['first'] = first_week
+    response['second'] = second_week
+
+    response.each do |w|
+      w[1].each do |d|
+        if d[1].is_a?Hash
+          d[1].delete_if { |k,v| v.empty?}
+        end
       end
     end
-    if false_check == 2
-      room << subj[0]
-      subj[0] = ''
-    elsif false_check == 3 and subj[0].is_lower?
-      room << subj[0]
-      subj[0] = ''
+    response
+  end
+
+  def group_pars(agent, page, name)
+    group = name
+    page = agent.page.link_with(:text => group).click
+    response = rasp_pars(page)
+    response
+  end
+
+  def teacher_pars(agent, page, name)
+    teacher = name
+    pp teacher.is_a?String
+    pp teacher.nil?
+    page = agent.page.link_with(:text => "Чугунов АВ").click
+    response = rasp_pars(page)
+    response
+  end
+
+  def aud_pars(agent, page, name)
+    aud = name
+    page = agent.page.link_with(:text => aud).click
+    response = rasp_pars(page)
+    response
+  end
+
+  def main(name, group=false, teacher=false, kafedra=false)
+    agent = Mechanize.new
+    if group
+      page = agent.get('http://rasp.sstu.ru/')
+      response = group_pars(agent, page, name)
+    elsif teacher
+      page = agent.get('http://rasp.sstu.ru/teacher')
+      response = teacher_pars(agent, page, name)
+    elsif aud
+      page = agent.get('http://rasp.sstu.ru/aud')
+      response = aud_pars(agent, page, name)
     end
-    dict['subject'] = subj
-    dict['room'] = room
-    dict
+    #page = agent.page.link_with(:text => group).click
+    hash = JSON["#{response.to_json}"]
+    JSON.pretty_generate(hash)
   end
 end
 
-def main(group)
-  response = Hash.new
-  first_week = Hash.new
-  second_week = Hash.new
-  agent = Mechanize.new
-  #group = gets.chomp
-  #group = 'б1-ИВЧТ41'
-  page = agent.get('http://rasp.sstu.ru/')
-  page = agent.page.link_with(:text => group).click
-  html = Nokogiri::HTML(page.body.force_encoding('UTF-8'))
-  rasp = html.css('div.text-center')[0]
+#m = "Чугунов АВ"
+#pars = Parser.new
+#puts pars.main(m.to_s, group=false, teacher=true, kafedra=false)
 
-  week = {1 => 'monday', 2 => 'tuesday', 3 => 'wednesday', 4 => 'thursday', 5 => 'friday', 6 => 'saturday'}
-
-  if rasp.content.split("\r\n")[1].strip.split(' ')[0] == 'Нечётная'
-    first_week['odd'] = true
-    flag = true
-  else
-    first_week['odd'] = false
-    flag = false
-  end
-  rasp = html.css('div.rasp-table-col')
-  a = []
-  rasp.each do |div|
-    answer = div.content.split("\r\n")
-    answer.each do |i|
-      answer[answer.index(i)] = i.strip
-    end
-    answer.delete_if(&:empty?)
-    #answer.delete_if{|i| i.include?':' }
-    answer.delete_at(0)
-    a.push(answer)
-  end
-
-# version with numbers
-=begin
-a[0..5].each do |list|
-  dict = {}
-  for i in [0, 2, 4, 6]
-    dict[list[i]] = get_pairs(list[i+1])
-  end
-  first_week[week[a.find_index(list)+1]] = dict
-end
-=end
-
-# version with time
-  a[0..5].each do |list|
-    dict = {}
-    for i in [0, 4, 8, 12]
-      dict[(list[i+1]+list[i+2]).gsub(' ', '')] = get_pairs(list[i+3])
-    end
-    first_week[week[a.find_index(list)+1]] = dict
-  end
-  pp first_week
-
-  if flag
-    second_week['odd'] = false
-  else
-    second_week['odd'] = true
-  end
-  index = 0
-  a[6..11].each do |list|
-    dict = {}
-    for i in [0, 4, 8, 12]
-      dict[(list[i+1]+list[i+2]).gsub(' ', '')] = get_pairs(list[i+3])
-    end
-    second_week[week[index+1]] = dict
-    index +=1
-  end
-
-  response['first'] = first_week
-  response['second'] = second_week
-
-  response.each do |w|
-    w[1].each do |d|
-      if d[1].is_a?Hash
-        d[1].delete_if { |k,v| v.empty?}
-      end
-    end
-  end
-
-  hash = JSON["#{response.to_json}"]
-  JSON.pretty_generate(hash)
-end
-
-main("б1-ИВЧТ41")
--- server.rb
+++ server.rb
@@ -1,7 +1,7 @@
 require 'sinatra'
 require_relative 'parser.rb'
 require 'json'
-
+require 'pp'
 
 get '/hi' do
   "Hello World!"
@@ -12,6 +12,31 @@ get '/group=:group' do |n|
   # params['name'] is 'foo' or 'bar'
   # n stores params['name']
   #JSON.pretty_generate(hash)
-  main(n)
+  Parser.new.main(n, group=true, teacher=false, kafedra=false)
 end
 
+get '/teacher=:teacher' do |m|
+  # matches "GET /hello/foo" and "GET /hello/bar"
+  # params['name'] is 'foo' or 'bar'
+  # n stores params['name']
+  #JSON.pretty_generate(hash) puts n
+  n = m.split('+').join(' ')
+  pp n
+  pp n.is_a?String
+  pp n.nil?
+  pars = Parser.new
+  pars.main("Чугунов АВ", group=false, teacher=true, kafedra=false)
+end
+
+get '/kafedra=:kafedra' do |n|
+  # matches "GET /hello/foo" and "GET /hello/bar"
+  # params['name'] is 'foo' or 'bar'
+  # n stores params['name']
+  #JSON.pretty_generate(hash)
+  Parser.new.main(n, group=false, teacher=false, kafedra=true)
+end
+
+get '/example.json' do
+  content_type :json
+  { :key1 => 'value1', :key2 => 'value2' }.to_json
+end
