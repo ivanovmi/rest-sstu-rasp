5e84dd582b8f923ce4fbd8495c10ffb3bc77b7cd
Mikhail Ivanov <mivanov@mirantis.com>
2015-09-15 19:50:39 +0300

    Remove unnecessary function

--- parser.rb
+++ parser.rb
@@ -72,21 +72,6 @@ class Parser
     end
   end
 
-  def symbolize_keys(hash)
-    hash.inject({}){|result, (key, value)|
-      new_key = case key
-                  when String then key.to_sym
-                  else key
-                end
-      new_value = case value
-                    when Hash then symbolize_keys(value)
-                    else value
-                  end
-      result[new_key] = new_value
-      result
-    }
-  end
-
   def rasp_pars(page, is_aud=false)
     response = Hash.new
     dict = Hash.new
